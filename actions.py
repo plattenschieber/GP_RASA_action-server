6e523cdd9c4ebb656905861580b7f37e963b1be0
leokalla <leokalla95@gmail.com>
2018-09-24 16:15:55 +0200

    Alle Pflichtfelder der Email werden gefüllt

--- actions.py
+++ actions.py
@@ -20,7 +20,6 @@ class ActionSendEmail(Action):
         # DialogueStateTracker:  the state tracker for the current user. You can access slot values using
         # tracker.get_slot(slot_name) and the most recent user message is tracker.latest_message.text
 
-        print("In Methode")
         # region data
         form_of_address = tracker.get_slot("form_of_address")
         first_name = tracker.get_slot("first_name")
@@ -31,33 +30,59 @@ class ActionSendEmail(Action):
         phone_number = tracker.get_slot("phone_number")
         e_mail = tracker.get_slot("e_mail")
         business_affair = tracker.get_slot("business_affair")
-        car_is_damaged = tracker.get_slot("car_is_damaged")
+
+        if tracker.get_slot("car_is_damaged") == True:
+            car_is_damaged = "Ja"
+        else:
+            car_is_damaged = "Nein"
+
         counterpart_is_insured = tracker.get_slot("counterpart_is_insured")
         damage_from_own_car = tracker.get_slot("damage_from_own_car")
         first_name_insured_party = tracker.get_slot("first_name_insured_party")
         surname_insured_party = tracker.get_slot("surname_insured_party")
         insurance_number = tracker.get_slot("insurance_number")
-        first_name_injured_party = tracker.get_slot("first_name_injured_party")
-        surname_injured_party = tracker.get_slot("surname_injured_party")
-        phone_number_injured_party = tracker.get_slot("phone_number_injured_party")
-        insurance_number_injured_party = tracker.get_slot("insurance_number_injured_party")
+        first_name_victim = tracker.get_slot("first_name_victim")
+        surname_victim = tracker.get_slot("surname_victim")
+        phone_number_victim = tracker.get_slot("phone_number_victim")
+        insurance_number_victim = tracker.get_slot("insurance_number_victim")
         insured_party_is_driver = tracker.get_slot("insured_party_is_driver")
         license_plate = tracker.get_slot("license_plate")
         date_of_damage = tracker.get_slot("date_of_damage")
+        time_of_damage = tracker.get_slot("time_of_damage")
         cause_of_damage = tracker.get_slot("cause_of_damage")
         damage_location = tracker.get_slot("damage_location")
         description_of_accident = tracker.get_slot("description_of_accident")
         current_location_of_car = tracker.get_slot("current_location_of_car")
-        is_callback_wanted = tracker.get_slot("is_callback_wanted")
-        # Überprüfung, wer der Fahrer war:
-        surname_of_driver = "Keine Angabe"
-        first_name_of_driver = "Keine Angabe"
-        if(insured_party_is_driver == True):
-            surname_of_driver = surname_insured_party
-            first_name_of_driver = first_name_insured_party
+        if tracker.get_slot("is_callback_wanted") == True:
+            callback_phone_number = tracker.get_slot("callback_phone_number")
         else:
-            surname_of_driver = "TBD: Name des Geschädigten"
-            first_name_of_driver = "TBD: Name des Geschädigten"
+            callback_phone_number = "-"
+        form_of_address_of_driver = tracker.get_slot("form_of_address_of_driver")
+        first_name_of_driver = tracker.get_slot("first_name_of_driver")
+        surname_of_driver = tracker.get_slot("surname_of_driver")
+        birth_date_of_driver = tracker.get_slot("birth_date_of_driver")
+        visible_damage_before = tracker.get_slot("visible_damage_before")
+        visible_damage_after = tracker.get_slot("visible_damage_after")
+        collateral_damage_claimed = tracker.get_slot("collateral_damage_claimed")
+        vorsteuerabzugsberechtigt = tracker.get_slot("vorsteuerabzugsberechtigt")
+        name_address_if_workshop = tracker.get_slot("name_address_if_workshop")
+        phone_number_if_workshop = tracker.get_slot("phone_number_if_workshop")
+        damage_report_by_if_not_insured = tracker.get_slot("damage_report_by_if_not_insured")
+        accident_caused_by = tracker.get_slot("accident_caused_by")
+        form_of_address_victim = tracker.get_slot("form_of_address_victim")
+        street_address_victim = tracker.get_slot("street_address_victim")
+        address_zip_code_victim = tracker.get_slot("address_zip_code_victim")
+        address_city_victim = tracker.get_slot("address_city_victim")
+        current_location_of_car_victim = tracker.get_slot("current_location_of_car_victim")
+        name_address_if_workshop_victim = tracker.get_slot("name_address_if_workshop_victim")
+        phone_number_if_workshop_victim = tracker.get_slot("phone_number_if_workshop_victim")
+        someone_injured = tracker.get_slot("someone_injured")
+        damage_report_by = tracker.get_slot("damage_report_by")
+
+        # Überprüfung, wer der Fahrer war:
+        if(insured_party_is_driver == True):
+            surname_of_driver = surname
+            first_name_of_driver = first_name
 
         # endregion data
         print("Daten geladen")
@@ -76,9 +101,30 @@ class ActionSendEmail(Action):
                                     damage_location=damage_location, cause_of_damage=cause_of_damage,
                                     description_of_accident=description_of_accident,
                                     first_name_of_driver=first_name_of_driver, surname_of_driver=surname_of_driver,
-                                    first_name_injured_party=first_name_injured_party,
-                                    surname_injured_party=surname_injured_party,
-                                    phone_number_injured_party=phone_number_injured_party)
+                                    first_name_victim=first_name_victim,
+                                    surname_victim=surname_victim,
+                                    phone_number_victim=phone_number_victim,
+                                    time_of_damage=time_of_damage, birth_date_of_driver=birth_date_of_driver,
+                                    form_of_address_of_driver=form_of_address_of_driver,
+                                    visible_damage_before=visible_damage_before,
+                                    visible_damage_after=visible_damage_after,
+                                    callback_phone_number=callback_phone_number,
+                                    collateral_damage_claimed=collateral_damage_claimed,
+                                    vorsteuerabzugsberechtigt=vorsteuerabzugsberechtigt,
+                                    name_address_if_workshop=name_address_if_workshop,
+                                    phone_number_if_workshop=phone_number_if_workshop,
+                                    damage_report_by_if_not_insured=damage_report_by_if_not_insured,
+                                    accident_caused_by=accident_caused_by,
+                                    form_of_address_victim=form_of_address_victim,
+                                    street_address_victim=street_address_victim,
+                                    address_zip_code_victim=address_zip_code_victim,
+                                    address_city_victim=address_city_victim,
+                                    current_location_of_car_victim=current_location_of_car_victim,
+                                    name_address_if_workshop_victim=name_address_if_workshop_victim,
+                                    phone_number_if_workshop_victim=phone_number_if_workshop_victim,
+                                    someone_injured=someone_injured,
+                                    damage_report_by=damage_report_by
+                                    )
 
         # setup the parameters of the message
         password = "GuidedProjectWS18/19"
@@ -231,6 +277,7 @@ class ActionAskKFZ(FormAction):
         return [
             EntityFormField("license_plate", "license_plate"),
             EntityFormField("date_of_damage", "date_of_damage"),
+            EntityFormField("time_of_damage", "time_of_damage"),
             EntityFormField("cause_of_damage", "cause_of_damage"),
             EntityFormField("damage_location", "damage_location"),
             EntityFormField("description_of_accident", "description_of_accident"),
